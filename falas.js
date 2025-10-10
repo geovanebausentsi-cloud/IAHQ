96cab9b8de31d5f138e8e6a3c9c24b3cf812075d
geovanebausentsi-cloud <geovanebausentsi@gmail.com>
2025-10-10 15:27:52 -0300

    feat: criação de algumas imagens modelo

---
+++ falas.js
@@ -0,0 +1,51 @@
+//ajuste as falas
+[
+    {
+        "cena": 1,
+        "character": "ITA",
+        "dialogue": "Aceitaria uma caipirinha?"
+    },
+    {
+        "cena": 1,
+        "character": "ITA",
+        "dialogue": "Aceito! :)"
+    },
+    {
+        "cena": 2,
+        "character": "MARI, ITA",
+        "action": "Silêncio.",
+        "sfx": "..."
+    },
+
+    {
+        "cena": 3,
+        "character": "ITA",
+        "dialogue": "Você não acha que... a cada dia a vida vem se tornando tão irreal, de um segundo pra cá tudo era feio e caótico, mas agora tudo está perfeito e em harmonia?",
+
+        "cena": 3,
+        "character": "Os Dois",
+        "action": "Silêncio.",
+
+        "cena": 3,
+        "character": "MARI",
+        "dialogue": "Harmonia... ou uma ilusão bem construída? Às vezes, a perfeição é o disfarce mais assustador." ,
+    },
+
+      {
+        "cena": 4,
+        "character": "ITA",
+        "dialogue": "Energia não se mente, as vezes devemos nos permitir confiar",
+
+        "cena": 4,
+        "character": "Mari",
+        "action": "Silêncio.",
+        "dialogue": "Nunca fui boa em confiar em ninguem.",
+
+        "cena": 4,
+        "character": "ITA",
+        "dialogue": "Ita, faz uma busca tentando hakiar Mari para saber mais sore ela, mais esta enfrentando bastante dificuldade" ,
+    }
+
+    
+]
+
